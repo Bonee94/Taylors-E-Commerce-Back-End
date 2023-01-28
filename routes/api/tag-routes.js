a1cb3e7800a93be86bc2161a66fb479dc83b08d9
Bonee94 <bonee94@yahoo.com>
2023-01-28 13:23:35 -0600

    Created DELETE route for deleting a tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -67,8 +67,24 @@ router.put("/:id", (req, res) => {
   })
 });
 
-router.delete("/:id", (req, res) => {
-  // delete on tag by its `id` value
+// delete on tag by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const tagDelete = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tagDelete) {
+      res.status(404).json({message: 'No Tag found with that id.'});
+      return;
+    };
+    
+    res.status(200).json({message: "Tag deleted"});
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
