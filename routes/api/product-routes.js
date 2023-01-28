40cd649187816fa4d50e0e12631357735d586149
Bonee94 <bonee94@yahoo.com>
2023-01-28 13:52:35 -0600

    Adjusted the Product.create response after creation to return the whole new product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,42 +1,42 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   // find all products including its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{model: Category}, {model: Tag}]
+      include: [{ model: Category }, { model: Tag }],
     });
     res.status(200).json(productData);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 // get one product
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id` and include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk( req.params.id, {
-      include: [{model: Category}, {model: Tag}]
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
     });
 
     if (!productData) {
-      res.status(404).json({message: 'No Product found with that id.'});
+      res.status(404).json({ message: "No Product found with that id." });
       return;
-    };
+    }
 
     res.status(200).json(productData);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -46,7 +46,7 @@ router.post('/', (req, res) => {
     }
   */
   Product.create(req.body)
-    .then((product) => {
+    .then(async (product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -55,12 +55,22 @@ router.post('/', (req, res) => {
             tag_id,
           };
         });
-        return ProductTag.bulkCreate(productTagIdArr);
+
+        await ProductTag.bulkCreate(productTagIdArr);
+
+        const productData = await Product.findByPk(product.id, {
+          include: [{ model: Category }, { model: Tag }],
+        });
+
+        return res.status(200).json(productData);
       }
       // if no product tags, just respond
-      res.status(200).json(product);
+      const productData = await Product.findByPk(product.id, {
+        include: [{ model: Category }, { model: Tag }],
+      });
+
+      res.status(200).json(productData);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -68,7 +78,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -109,23 +119,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
       where: {
         id: req.params.id,
-      }
+      },
     });
 
     if (!productData) {
-      res.status(404).json({message: 'No Product found with that id.'});
+      res.status(404).json({ message: "No Product found with that id." });
       return;
-    };
+    }
 
-    res.status(200).json({message: "Product deleted"});
+    res.status(200).json({ message: "Product deleted" });
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
